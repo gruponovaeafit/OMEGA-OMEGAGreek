373efc9ff451f0b1c78eca85f6de3e2012627be9
NoprauxX12 <juanselizca@outlook.com>
2025-04-01 00:07:16 -0500

    Loading page timeout implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-import React from "react";
-
+"use client";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 
 export default function Loading() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/home');
+    }, 5000); 
+    return () => clearTimeout(timer); 
+  }, [router]); 
+
     return (
       <div className="background min-h-screen flex flex-col justify-center items-center py-4">
         <img src="/Logo.svg" alt="" className="w-40 h-auto"/>
