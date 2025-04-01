b9534727296a2e0d3942d4c958b98c08686a8820
NoprauxX12 <juanselizca@outlook.com>
2025-03-31 23:58:04 -0500

    Loading animation implemented

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+export default function Loading() {
+    return (
+      <div className="background min-h-screen flex flex-col justify-center items-center py-4">
+        <img src="/Logo.svg" alt="" className="w-40 h-auto"/>
+      </div>
+    )
+}
