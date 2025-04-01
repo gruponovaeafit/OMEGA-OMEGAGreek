ec0f20400b8df05ed5a56cf81b0ea4451366b692
juanrobles05 <116527097+juanrobles05@users.noreply.github.com>
2025-04-01 10:46:12 -0500

    teamInscription api created

--- src/pages/api/forms/teamInscriptionForm.ts
+++ src/pages/api/forms/teamInscriptionForm.ts
@@ -3,15 +3,64 @@ import { connectToDatabase } from "../db";
 
 
 
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
 
-    //Conexión con la db     
+    //Db connection
     try {
-        const pool = await connectToDatabase();
-        res.status(200).json({ message: "Conexión exitosa" });
-      } catch (error) {
-        res.status(500).json({ error: "Error conectando a la base de datos" });
-    }
-    
+        if (req.method !== "POST") {
+            return res.status(405).json({ message: "Método no permitido" });
+        }
+        // const pool = await connectToDatabase();
+        // res.status(200).json({ message: "Conexión exitosa" });
 
-}  
+        // Get data from the request body;
+        const { team_name, leader_email} = req.body;
+        console.log("Datos recibidos:", req.body);
+
+        // Validate that the data is not empty
+        if (!team_name || !leader_email) {
+            return res.status(400).json({ error: "Faltan datos requeridos" });
+        }
+
+        // Validate that the email is in the correct format
+        if (!leader_email || !leader_email.endsWith("@eafit.edu.co")) {
+            console.error("Correo inválido o faltante:", { leader_email });
+            return res.status(400).json({
+              notification: {
+                type: "error",
+                message: "El correo debe ser del dominio @eafit.edu.co.",
+              },
+            });
+        }
+
+        // Validate that the email exist in the database
+        // const pool = await connectToDatabase();
+        // const result = await pool.request()
+        //     .input("leader_email", leader_email)
+        //     .query("SELECT * FROM Users WHERE institutional_email = @leader_email");
+        // const user = result.recordset[0];
+        // if (!user) {
+        //     return res.status(400).json({ error: "El correo no existe en la base de datos" });
+        // }
+
+        // try {
+        //     const timestamp = new global.Date(); // Get the current date and time
+        //     pool.request()
+        //         .input("team_name", team_name)
+        //         .input("leader_email", leader_email)
+        //         .input("timestamp", timestamp)
+        //         .query("INSERT INTO Teams (team_name, leader_email, created_at) VALUES (@team_name, @leader_email, @timestamp)");
+        //         console.log("Equipo insertado:", { team_name, leader_email, timestamp });
+
+        //         return res.status(200).json({ message: "Inscripción exitosa" });
+
+        // } catch (error) {
+        //     return res.status(400).json({ error: "Error al insertar en la base de datos" });
+        //   }
+
+    } catch (error) {
+            console.error("Error al insertar en la base de datos:", error);
+            res.status(500).json({ error: "Error al insertar en la base de datos" });
+      }
+}
