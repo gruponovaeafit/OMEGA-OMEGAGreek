78f183983496f966d7c6da9567dbac15cad67fd1
Alejoso <atirador1@eafit.edu.co>
2025-04-14 22:28:12 -0500

    Feature: Modified teamEmailConfirmation to create the JWT token with the validated leader email

--- src/pages/api/forms/teamEmailConfirmation.ts
+++ src/pages/api/forms/teamEmailConfirmation.ts
@@ -1,5 +1,6 @@
 //Validate that the email is in Teams_data
-
+import jwt from "jsonwebtoken";
+import { serialize } from "cookie";
 import { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from "../db";
 import { Int, Date as SqlDate, TinyInt, VarChar } from "mssql";
@@ -7,24 +8,24 @@ import { getEmailFromCookies } from "../getEmailFromCookies";
 import verifyRecaptchaEnterprise from "../verifyRecaptchaEnterprise";
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
+    req: NextApiRequest,
+    res: NextApiResponse,
 
 ) {
     if (req.method !== "POST") {
         return res.status(405).json({ message: "Método no permitido" });
     }
 
-    const {recaptchaToken , leader_email} = req.body;
+    const { recaptchaToken, leader_email } = req.body;
 
     // Validate data
-    if(!leader_email){
+    if (!leader_email) {
         return res.status(400).json({
             notification: {
-              type: "error",
-              message: "No se ingreso ningun correo.",
+                type: "error",
+                message: "No se ingreso ningun correo.",
             },
-          });
+        });
     }
 
     // Ask if we have to send to another url if the recaptcha fails
@@ -34,72 +35,86 @@ export default async function handler(
     if (!isHuman) {
         return res.status(400).json({
             notification: {
-              type: "error",
-              message: "Validación reCAPTCHA fallida. Intenta de nuevo.",
+                type: "error",
+                message: "Validación reCAPTCHA fallida. Intenta de nuevo.",
             },
         });
     }
-    //Look if we are going to need this part or create a new jwt token
-    // Get the email from the cookies
-    const userEmail = getEmailFromCookies(req, res);
-    if (!userEmail) {
-        return res.status(400).json({
-            notification: {
-                type: "error",
-                message: "Validación reCAPTCHA fallida. Intenta de nuevo.",
-              },
-            redirectUrl: "/" });
-    }
+
+   
 
     try {
 
         const pool = await connectToDatabase();
 
-        try
-        {
+        try {
             const isThereLeaderEmail = await pool
                 .request()
                 .input("leader_email", VarChar(100), leader_email).query(`
-                SELECT from Teams_data
+                SELECT leader_email from Teams_data
                 WHERE leader_email = @leader_email
                 `);
 
             if (isThereLeaderEmail.recordset.length === 0) {
                 return res.status(400).json({
                     notification: {
-                      type: "error",
-                      message: "El correo no es de un lider.",
+                        type: "error",
+                        message: "El correo no es de un lider.",
                     },
-                  });
+                });
 
-                }
+            } else
 
-            return res.status(200).json({
-                notification: {
-                type: "info",
-                message: "El correo es valido.",
-                redirectUrl: "/teamNameConfirmation",
-                }
-            });
+            {
+                const leaderEmailLower = leader_email.toLowerCase();
+                // Generar JWT
+                const jwtToken = jwt.sign(
+                    { email: leaderEmailLower},
+                    process.env.JWT_KEY as string,
+                    {
+                        expiresIn: "20m",
+                    },
+                );
+                // Generate Cookies and store JWT in it
+                res.setHeader(
+                    "Set-Cookie",
+                    serialize("jwtToken", jwtToken, {
+                        httpOnly: true,
+                        secure: process.env.NODE_ENV !== "development",
+                        maxAge: 3600,
+                        path: "/",
+                        sameSite: "strict",
+                    }),
+                );
+
+                return res.status(200).json({
+                    notification: {
+                        type: "info",
+                        message: "El correo es valido.",
+                        redirectUrl: "/teamNameConfirmation",
+                    }
+                });
+            }
+
         }
-            catch (error) {
+        catch (error) {
             console.error("❌ Error al consultar el correo:", error);
             return res.status(500).json({
                 notification: {
-                type: "error",
-                message: "Error al consultar el correo",
+                    type: "error",
+                    message: "Error al consultar el correo",
                 },
             });
         }
 
-      } catch (error) {
+    } catch (error) {
         console.error("❌ Error al conectar a la base de datos:", error);
         return res.status(500).json({
-          notification: {
-            type: "error",
-            message: "Error al conectar a la base de datos",
-          },
+            notification: {
+                type: "error",
+                message: "Error al conectar a la base de datos",
+            },
         });
-      }
+    }
 
 }
