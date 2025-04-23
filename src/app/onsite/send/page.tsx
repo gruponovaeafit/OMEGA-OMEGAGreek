de7b9169ea33091fac41b5f17d17cab895f3eea7
NoprauxX12 <juanselizca@outlook.com>
2025-04-23 18:41:33 -0500

    Send Onsite form

--- src/app/onsite/send/page.tsx
+++ src/app/onsite/send/page.tsx
@@ -1,3 +1,14 @@
-export default function Confirmation2Page() {
-  return <div>Confirmation 2</div>;
+import { Footer } from "@/app/components/Footer";
+import { Header } from "@/app/components/Header";
+
+
+export default function Send() {
+  return(
+    <div className="background_email flex flex-col items-center justify-center gap-6 px-4 min-h-screen">
+      <Header />
+        <img src="/Onsite_send.svg" alt="" />
+        <img src="/APOLO_2.svg" alt="" />
+      <Footer />
+    </div>
+  )
 }
