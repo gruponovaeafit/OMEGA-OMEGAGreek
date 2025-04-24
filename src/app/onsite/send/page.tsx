9ffa90c3e1febebc4966c86cc7c13d9347109c92
NoprauxX12 <juanselizca@outlook.com>
2025-04-23 19:05:49 -0500

    Onsite send form corrected

--- src/app/onsite/send/page.tsx
+++ src/app/onsite/send/page.tsx
@@ -1,14 +1,19 @@
 import { Footer } from "@/app/components/Footer";
 import { Header } from "@/app/components/Header";
 
-
 export default function Send() {
-  return(
-    <div className="background_email flex flex-col items-center justify-center gap-6 px-4 min-h-screen">
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-6 px-4 background_email relative">
       <Header />
+
+      <div className="absolute top-20 flex flex-col items-center">
         <img src="/Onsite_send.svg" alt="" />
-        <img src="/APOLO_2.svg" alt="" /> 
+      </div>
+      <div className="mt-52">
+        <img src="/APOLO_2.svg" alt="Apolo confirmación" className="w-60" />
+      </div>
+
       <Footer />
     </div>
-  )
+  );
 }
